e70d29712c5e01ba80d9bf4344aeba70cc1b1d49
TheLKL321 <lkl.zarebski@gmail.com>
2018-03-12 22:50:16 +0100

    main template

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void addUser (unsigned int parentUserId, unsigned int userId) {
+	
+	printf("OK\n");
+}
+
+void delUser (unsigned int userId) {
+
+}
+
+void addMovie (unsigned int userId, long movieRating) {
+
+}
+
+void delMovie (unsigned int userId, long movieRating) {
+
+}
+
+void marathon (unsigned int userId, long k) {
+
+}
+
+int main () {
+	return 0;
+}
