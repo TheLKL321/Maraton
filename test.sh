d81df4b099b23da026524ab1c87f7df9bf256fa0
TheLKL321 <lkl.zarebski@gmail.com>
2018-03-27 15:42:39 +0200

    add tests

--- test.sh
+++ test.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+for f in testy/*.in
+do
+	./main <$f >${f%in}out 2>${f%in}err
+	
+  	if diff -w ${f%in}out ${f%.in}_wyn.out
+	then
+		echo "${f%.in} out passed"
+	else
+		echo "${f%.in} out failed"
+	fi
+
+	if diff -w ${f%in}err ${f%.in}_wyn.err
+	then
+		echo "${f%.in} err passed"
+	else
+		echo "${f%.in} err failed"
+	fi
+
+  	echo "Kod: $?."
+done
