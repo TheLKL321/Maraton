d1c633e406480922170bf06701301c6995581408
TheLKL321 <lkl.zarebski@gmail.com>
2018-04-01 17:55:35 +0200

    improve script

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+pass=0
+fail=0
+
 for f in testy/*.in
 do
 	TMPFILE1="$(mktemp)"
@@ -10,16 +13,23 @@ do
   	if diff ${f%in}out $TMPFILE1
 	then
 		echo "${f%.in} out passed"
+		let "pass += 1"
 	else
 		echo "${f%.in} out failed"
+		let "fail += 1"
 	fi
 
 	if diff ${f%in}err $TMPFILE2
 	then
 		echo "${f%.in} err passed"
+		let "pass += 1"
 	else
 		echo "${f%.in} err failed"
+		let "fail += 1"
 	fi
 
   	echo "Kod: $?."
 done
+
+echo "Passed files: $pass"
+echo "Failed files: $fail"
